13cb30654e89be8f2c3f2d9983ac2593b0c9d23e
kunek29 <andrzej.kunecki@gmail.com>
2024-01-27 19:05:30 +0100

    Add search form and results page with links

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 from . import util
 from django import forms
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from re import search
 
 
 def index(request):
@@ -14,5 +17,26 @@ def entry_page(request, result):
     return render(request, "encyclopedia/entry_page.html", {
         "title": result,
         "body": util.get_entry(result)
+    })
+
+def results(request):
+    result = request.GET.get('q')
+    result = result.lower()
+
+    entries = util.list_entries()
+
+    result_list = []
+
+    for entry in entries:
+        entry = entry.lower()
+
+        if result == entry:
+            return entry_page(request, result)        
+        elif result in entry:
+            result_list.append(entry)
+            print(result_list)
+        
+    return render(request, "encyclopedia/results.html", {
+        "entries": result_list
     })
 
